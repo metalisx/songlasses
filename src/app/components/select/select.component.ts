936e004c6ed05f90ab16050a5b7ae20a4289d7ce
stefan <sa.oudenijhuis@caiway.nl>
2021-04-27 22:51:52 +0200

    display config in example page

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -45,27 +45,25 @@ export class SelectComponent implements OnInit {
   value?: string = 'value2';
   valueStyled?: string = 'value2';
 
-  public styles: string = `
-  <style>
-    .select2.sg-select {
-      width: 50%;
-    }
-    .select2.sg-select sg-select-input {
-      border: 1px solid #904040;
-    }
-    .select2.sg-select .sg-select-items {
-      border: 1px solid #904040;
-    }
-    .select2.sg-select .sg-select-items .sg-select-item {
-      background-color: #904040;
-      color: #fff;
-    }
-    .select2.sg-select .sg-select-items .sg-select-item:hover {
-      background-color: #803030;
-      color: #fff;
-    }
-  </style>
-  `;
+  public styles: string = `<style>
+  .select2.sg-select {
+    width: 50%;
+  }
+  .select2.sg-select sg-select-input {
+    border: 1px solid #904040;
+  }
+  .select2.sg-select .sg-select-items {
+    border: 1px solid #904040;
+  }
+  .select2.sg-select .sg-select-items .sg-select-item {
+    background-color: #904040;
+    color: #fff;
+  }
+  .select2.sg-select .sg-select-items .sg-select-item:hover {
+    background-color: #803030;
+    color: #fff;
+  }
+</style>`;
   public stylesSafeHtml?: SafeHtml;
   
   constructor(private sanitizer: DomSanitizer) { }
