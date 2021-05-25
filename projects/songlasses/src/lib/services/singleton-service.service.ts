26a235e836307f27de261c7c545407fcb81e3f2f
stefan <sa.oudenijhuis@caiway.nl>
2021-05-25 23:32:47 +0200

    added null check

--- projects/songlasses/src/lib/services/singleton-service.service.ts
+++ projects/songlasses/src/lib/services/singleton-service.service.ts
@@ -3,7 +3,7 @@ import { inject, InjectFlags, Type } from "@angular/core";
 export class SingletonService<T> {
 
     constructor(type: Type<T>) {
-      if (inject(type, InjectFlags.Optional | InjectFlags.SkipSelf)) {
+      if (inject(type, InjectFlags.Optional | InjectFlags.SkipSelf) !== null) {
         throw Error(`Trying to create multiple instances of singleton service ${type.name}.`);
       }
     }
