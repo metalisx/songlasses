76e5fa2b78abbf3776c5e0d832444256a91fae51
stefan <sa.oudenijhuis@caiway.nl>
2021-09-25 14:21:52 +0200

    updated story for SgSelect

--- src/stories/SgSelect.stories.ts
+++ src/stories/SgSelect.stories.ts
@@ -1,21 +1,17 @@
 // also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { moduleMetadata } from '@storybook/angular';
 import { Story, Meta } from '@storybook/angular/types-6-0';
 import { SgSelectComponent } from 'projects/songlasses/src/lib/components/sg-select/sg-select.component';
 import { SgSelectModule } from 'projects/songlasses/src/lib/components/sg-select/sg-select.module';
-import { SgSelectComponentService } from 'projects/songlasses/src/lib/services/sg-component/sg-select-component.service';
-import { SgRootComponentService } from 'projects/songlasses/src/lib/services/sg-component/sg-root-component.service';
-import { SgGroupComponentService } from 'projects/songlasses/src/lib/services/sg-component/sg-group-component.service';
 
 export default {
   title: 'Songlasses/SgSelect',
   component: SgSelectComponent,
   decorators: [
     moduleMetadata({
-      imports: [CommonModule, RouterModule, SgSelectModule],
-      providers: [SgSelectComponentService, SgRootComponentService, SgGroupComponentService]
+      imports: [CommonModule, FormsModule, SgSelectModule]
     })
   ],
 } as Meta;
@@ -25,3 +21,8 @@ const Template: Story<SgSelectComponent> = (args: SgSelectComponent) => ({
 });
 
 export const Default = Template.bind({});
+Default.args = {
+  componentConfig: {
+    
+  }
+} 
