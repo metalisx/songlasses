dacc098b7f259026320951c5c49ddc5b32103d3f
stefan <sa.oudenijhuis@caiway.nl>
2021-09-27 00:21:59 +0200

    more stories for select

--- src/stories/SgSelect.stories.ts
+++ src/stories/SgSelect.stories.ts
@@ -15,27 +15,79 @@ export default {
     moduleMetadata({
       imports: [CommonModule, FormsModule, SgSelectModule]
     })
-  ],
+  ]
 } as Meta;
 
 const Template: Story<SgSelectComponent> = (args: SgSelectComponent) => ({
   props: args
 });
 
-export const Default = Template.bind({});
-Default.args = {
+export const Defaults = Template.bind({});
+Defaults.args = {
   componentConfig: {
+    name: 'select',
+    show: true,
+    required: true,
+    itemMatchStrategy: 'startsWith',
+    itemValueField: 'value',
+    itemDescriptionField: 'description',
+    items: [],
+    className: ''
+  }
+} 
+
+export const Minimal = Template.bind({});
+Minimal.args = {
+  componentConfig: {
+    name: 'minimalSelect',
     items: MOVIESTARS
   }
 } 
 
-export const AllOptions = Template.bind({});
-AllOptions.args = {
+export const ShowIsFalse = Template.bind({});
+ShowIsFalse.args = {
   componentConfig: {
+    name: 'showIsFalseSelect',
+    required: false,
+    items: MOVIESTARS
+  }
+}
+
+export const RequiredIsFalse = Template.bind({});
+RequiredIsFalse.args = {
+  componentConfig: {
+    name: 'requiredIsFalseSelect',
+    required: false,
+    items: MOVIESTARS
+  }
+}
+
+export const ItemMatchStrategyIsContains = Template.bind({});
+ItemMatchStrategyIsContains.args = {
+  componentConfig: {
+    name: 'itemMatchStrategyIsContainsSelect',
+    itemMatchStrategy: 'contains',
+    items: MOVIESTARS,
+  }
+}
+
+export const ItemMatchStrategyIsStartsWith = Template.bind({});
+ItemMatchStrategyIsStartsWith.args = {
+  componentConfig: {
+    name: 'itemMatchStrategyIsStartsWithSelect',
     itemMatchStrategy: 'startsWith',
-    required: true,
+    items: MOVIESTARS,
+  }
+}
+
+export const CustomItemFields = Template.bind({});
+CustomItemFields.args = {
+  componentConfig: {
+    name: 'customItemFieldsSelect',
     itemValueField: 'id',
     itemDescriptionField: 'name',
     items: SUPERHEROES
   }
-} 
+}
+
+
